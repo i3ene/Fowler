2207de6524bb60757803848b2e9404a445378b37
I3ene <benedikt.muell@t-online.de>
2023-05-09 14:56:30 +0200

    renamed variables to clarify code

--- src/Customer.java
+++ src/Customer.java
@@ -43,27 +43,27 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental rental) {
+        double total = 0;
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                total += 2;
+                if (rental.getDaysRented() > 2)
+                    total += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                total += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                total += 1.5;
+                if (rental.getDaysRented() > 3)
+                    total += (rental.getDaysRented() - 3) * 1.5;
                 break;
             default:
                 System.out.println("Error in Switchcase");
                 break;
         }
-        return thisAmount;
+        return total;
     }
 
 }
